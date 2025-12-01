c79013d2751dd3e29873895ae73749430bacdef2
Mark Johnson <markjohn44@gmail.com>
2025-12-01 14:49:31 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.rst").read_text()
 
 setup(
-    long_description=long_description
+   # long_description=long_description
+    version="0.1.1",
 )
